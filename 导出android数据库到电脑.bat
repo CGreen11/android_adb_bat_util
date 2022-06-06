54bfd2f91282d335fcc9c07ceb7b75b51e59ad6c
CGreen11 <38218519+CGreen11@users.noreply.github.com>
2022-06-06 15:53:45 +0800

    Update 导出android数据库到电脑.bat

--- 导出android数据库到电脑.bat
+++ 导出android数据库到电脑.bat
@@ -1,7 +1,10 @@
 :: 中文乱码
 chcp 65001
 
-:: android的ip地址
+:: 0：网络连接，非0：直连
+SET connect_flag=1
+
+:: 网络连接 ： android的ip地址
 SET devices_ip=192.168.2.20 
 
 :: 所有日志输出到电脑的位置，非中文，结尾不带分隔符
@@ -12,9 +15,6 @@ SET db_path=/data/data/test/databases/
 
 :: android的数据库名
 SET db_name=test
-
-:: 0：网络连接，非0：直连
-SET connect_flag=1
 
 :: 运行逻辑
 call shell/pull_db.bat
