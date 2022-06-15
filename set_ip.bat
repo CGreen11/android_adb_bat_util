f90f92db4c44207b3e5f8a30740e799e937513fc
CGreen11 <38218519+CGreen11@users.noreply.github.com>
2022-06-15 09:31:50 +0800

    Delete set_ip.bat

--- set_ip.bat
+++
@@ -1,8 +0,0 @@
-:: 0：网络连接，非0：直连
-IF %connect_flag%==0 (adb disconnect&&adb connect %devices_ip%)
-
-adb shell setprop persist.net.eth1.mode 1    
-
-adb shell setprop persist.net.eth1.staticinfo %new_ip%,24,%gateway%,114.114.114.114,219.141.136.10
-
-adb reboot
